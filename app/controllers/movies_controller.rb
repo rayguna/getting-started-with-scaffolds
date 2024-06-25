3e1a66ba19f29e05cece7afe6875482b8d0ce05d
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-06-25 18:04:00 +0000

    Trying to get the delete link to work.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -50,7 +50,7 @@ class MoviesController < ApplicationController
 
   def destroy
     the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_movie = Movie.where({ :id => the_id })[0] #.at(0)
 
     the_movie.destroy
 
